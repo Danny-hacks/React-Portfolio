72dfd7b315461dd5149c3d5cd393c9ac24aa6032
Danny-hacks <obiefuledaniel@gmail.com>
2022-09-10 04:42:27 +0100

    animations-edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Header from './components/header/header';
 import Nav from './components/nav/nav';
 import About from './components/about/about';
@@ -7,18 +7,23 @@ import Services from './components/services/services';
 import Portfolio from './components/portfolio/portfolio';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 
 const App = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000});
+  }, []);
   return ( <>
-    <Header/>
+    <div data-aos="fade-up"><Header/></div>
     <Nav/>
-    <About/>
-    <Experience/>
-    <Services/>
-    <Portfolio/>
-    <Contact/>
-    <Footer/>
+    <div data-aos="fade-up"><About/></div>
+    <div data-aos="fade-up"><Experience/></div>
+    <div data-aos="fade-up"><Services/></div>
+    <div data-aos="fade-up"><Portfolio/></div>
+    <div data-aos="fade-up"><Contact/></div>
+    <div data-aos="fade-up"><Footer/></div>
 </>
   )
 }
