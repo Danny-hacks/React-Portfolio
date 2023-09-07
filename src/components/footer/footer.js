d5791a625bd4156471b334eedae5a7780fe2d547
Maestro <81395117+Danny-hacks@users.noreply.github.com>
2023-09-07 13:52:23 +0100

    updated resume and minor changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import './footer.css';
-import {IoLogoTwitter} from 'react-icons/io';
-import {BsLinkedin} from 'react-icons/bs';
+import React from "react";
+import "./footer.css";
+import { IoLogoTwitter } from "react-icons/io";
+import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer_logo'>Maestro</a>
+      <a href='#' className='footer_logo'>
+        Maestro
+      </a>
 
-      <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <ul className='permalinks'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
       </ul>
 
-      <div className="footer_socials">
-        <a href="https://twitter.com"><IoLogoTwitter/></a>
-        <a href="https://linkdin.com" target="_blank"><BsLinkedin/></a>
+      <div className='footer_socials'>
+        {/* <a href="https://twitter.com"><IoLogoTwitter/></a> */}
+        <a href='https://linkdin.com' target='_blank'>
+          <BsLinkedin />
+        </a>
       </div>
 
-      <div className="footer_copyright">
+      <div className='footer_copyright'>
         <small>&copy; Maestro. All rights reserved</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
