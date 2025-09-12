cf7735e14d57df8ef1def57a93480b3ebc94e70e
Maestro <81395117+Danny-hacks@users.noreply.github.com>
2025-09-12 05:52:17 +0100

    design-updates

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import Cta from "./CTA";
 import Me from "../../assets/me.jpg";
 import HeaderSocials from "./headerSocials";
-import Typewriter from "./Typewriter";
 
 const Header = () => {
+  const [greetingVisible, setGreetingVisible] = useState(false);
+  const [nameVisible, setNameVisible] = useState(false);
+  const [titleVisible, setTitleVisible] = useState(false);
+  const [greetingFinished, setGreetingFinished] = useState(false);
+  const [nameFinished, setNameFinished] = useState(false);
+
+  useEffect(() => {
+    // Start greeting animation after initial delay
+    const greetingTimer = setTimeout(() => {
+      setGreetingVisible(true);
+    }, 500);
+
+    // Start name animation after greeting
+    const nameTimer = setTimeout(() => {
+      setGreetingFinished(true);
+      setNameVisible(true);
+    }, 2500);
+
+    // Start title animation after name
+    const titleTimer = setTimeout(() => {
+      setNameFinished(true);
+      setTitleVisible(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(greetingTimer);
+      clearTimeout(nameTimer);
+      clearTimeout(titleTimer);
+    };
+  }, []);
+
   return (
     <header>
       <div className='container header_container'>
-        <h5>Hello, I'm</h5>
-        <h1 className='fade-in'>Daniel Obiefule</h1>
-        <h5 className='text-light'>
-          <Typewriter text='Software Engineer - Web Developer' />
+        <h5
+          className={`typewriter-line greeting ${
+            greetingVisible ? "typing" : ""
+          } ${greetingFinished ? "finished" : ""}`}
+        >
+          Hello, I'm
         </h5>
+
+        <h1
+          className={`typewriter-line name ${nameVisible ? "typing" : ""} ${
+            nameFinished ? "finished" : ""
+          }`}
+        >
+          Daniel Obiefule
+        </h1>
+
+        <h5
+          className={`text-light typewriter-line title ${
+            titleVisible ? "typing" : ""
+          }`}
+        >
+          Software Engineer - Web Developer
+        </h5>
+
         <Cta />
         <HeaderSocials />
 
