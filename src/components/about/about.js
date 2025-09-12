0f871715863a9dd3f529811396377a5a21e45234
Maestro <81395117+Danny-hacks@users.noreply.github.com>
2025-09-12 04:23:33 +0100

    design-changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,40 +5,51 @@ import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
 
-const about = () => {
+const highlights = [
+  {
+    icon: <FaAward className='about_icon' />,
+    title: "Experience",
+    subtitle: "2019 - Present",
+  },
+  {
+    icon: <FiUsers className='about_icon' />,
+    title: "Clients",
+    subtitle: "Worked With Multiple Clients",
+  },
+  {
+    icon: <VscFolderLibrary className='about_icon' />,
+    title: "Projects",
+    subtitle: "Completed Multiple Projects",
+  },
+];
+
+const About = () => {
   return (
     <section id='about'>
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h5 className='section_subtitle'>Get To Know</h5>
+      <h2 className='section_title'>About Me</h2>
 
       <div className='container about_container'>
+        {/* IMAGE SIDE */}
         <div className='about_me'>
           <div className='about_me-image'>
-            <img src={ME} alt='about Image' />
+            <img src={ME} alt='About' />
           </div>
         </div>
+
+        {/* CONTENT SIDE */}
         <div className='about_content'>
-          <div className='about_cards'>
-            <article className='about_card'>
-              <FaAward className='about_icon' />
-              <h5>Experience</h5>
-              <small>2019 - till date</small>
-            </article>
-
-            <article className='about_card'>
-              <FiUsers className='about_icon' />
-              <h5>Clients</h5>
-              <small>Worked With Multiple Clients</small>
-            </article>
-
-            <article className='about_card'>
-              <VscFolderLibrary className='about_icon' />
-              <h5>Projects</h5>
-              <small>Completed Multiple Projects </small>
-            </article>
+          <div className='about_highlights'>
+            {highlights.map((item, index) => (
+              <article key={index} className='about_card'>
+                {item.icon}
+                <h5>{item.title}</h5>
+                <small>{item.subtitle}</small>
+              </article>
+            ))}
           </div>
 
-          <p>
+          <p className='about_description'>
             I'm a front-end developer and web designer specializing in
             SEO-friendly websites using WordPress, Elementor, and WooCommerce. I
             build high-converting funnels and landing pages with GoHighLevel,
@@ -46,8 +57,8 @@ const about = () => {
             automation, tracking, and performance optimization.
           </p>
 
-          <a href='#contact' className='btn btn-primary'>
-            Let's Talk
+          <a href='#contact' className='btn btn-primary modern_btn'>
+            Let’s Talk
           </a>
         </div>
       </div>
@@ -55,4 +66,4 @@ const about = () => {
   );
 };
 
-export default about;
+export default About;
